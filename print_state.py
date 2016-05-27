b49b7b9b5ef14bcc238c444a7c1b3c0fa9f40f58
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2016-05-27 15:52:02 +0200

    test the configuration

---
+++ print_state.py
@@ -0,0 +1,89 @@
+from classes import *
+import sys, select, os, array
+from array import array
+#import ROOT
+#from ROOT import TGraph
+
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option('-s', '--setting', metavar='F', type='string', action='store',
+default	=	'default',
+dest	=	'setting',
+help	=	'configuration setting ie default')
+
+parser.add_option('-n', '--number', metavar='F', type='int', action='store',
+default	=	1,
+dest	=	'number',
+help	=	'configuration number')
+
+parser.add_option('-m', '--mpa', metavar='F', type='int', action='store',
+default	=	0,
+dest	=	'mpa',
+help	=	'mpa to configure (0 for all)')
+
+(options, args) = parser.parse_args()
+
+
+a = uasic(connection="file://connections_test.xml",device="board0")
+mapsa = MAPSA(a)
+
+read = a._hw.getNode("Control").getNode('firm_ver').read()
+a._hw.dispatch()
+print "Running firmware version " + str(read)
+a._hw.getNode("Configuration").getNode('num_MPA').write(6)
+a._hw.dispatch()
+read = a._hw.getNode("Configuration").getNode('num_MPA').read()
+a._hw.dispatch()
+print "num_MPA " + str(read)
+
+read= self._Utility.getNode('MPA_settings').getNode("VDDPST_enable").read()
+a._hw.dispatch()
+print "VDDPST_enable" + str(read)
+read= self._Utility.getNode('MPA_settings').getNode("DVDD_enable").read()
+a._hw.dispatch()
+print "DVDD_enable" + str(read)
+read= self._Utility.getNode('MPA_settings').getNode("AVDD_enable").read()
+a._hw.dispatch()
+print "AVDD_enable" + str(read)
+read= self._Utility.getNode('MPA_settings').getNode("PVDD_enable").read()
+a._hw.dispatch()
+print "PVDD_enable" + str(read)
+read= self._Utility.getNode('MPA_settings').getNode("VBIAS_enable").read()
+a._hw.dispatch()
+print "VBIAS_enable" + str(read)
+read= self._Utility.getNode("DAC_register").read()
+a._hw.dispatch()
+print "DAC_register" + str(read)
+
+mpa_number = options.mpa
+if mpa_number ==0:
+	config = mapsa.config(Config=options.number,string=options.setting)
+	config.upload(show = 0)
+	config.write()
+	print ""
+	print "checking config"
+	for i in range(1,7):
+		print i
+       		read = a._hw.getNode("Configuration").getNode("Memory_OutConf").getNode("MPA"+str(i)).getNode("config_1").read()	
+		a._hw.dispatch()
+		print read
+else:
+	mpa_index = mpa_number-1
+	
+	mpa = []  
+	for i in range(1,7):
+		mpa.append(mapsa.getMPA(i))
+
+	Confnum=options.number
+	configarr = []
+
+	writesetting=6-mpa_number
+
+	print "Configuring MPA number " + str(mpa_number)
+
+	curconf = mpa[mpa_index].config(xmlfile="data/Conf_"+options.setting+"_MPA"+str(mpa_number)+"_config"+str(Confnum)+".xml")
+	curconf.upload()
+	a._hw.dispatch()
+	
+	print ""
+	print "Done"
