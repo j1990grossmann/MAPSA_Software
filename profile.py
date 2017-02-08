3cddd34bb94020f0757d88ced6a69653e1e7203c
Johannes Grossmann <Johannes.Grossmann@cern.ch>
2017-02-08 16:47:44 +0100

    added extra parameters

--- profile.py
+++ profile.py
@@ -3,7 +3,7 @@ import re
 import pstats
 import daq_continous_2MPA
 
-cProfile.run('daq_continous_2MPA.start_daq()', 'restats')
+cProfile.run('daq_continous_2MPA.start_daq(2,100)', 'restats')
 p = pstats.Stats('restats')
 p.strip_dirs().sort_stats("cum").print_stats()
 
